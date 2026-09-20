136571d1a1dbcb651b73186337c8bf7ab79f0f1d
GodOfSpid <egra1egor@gmail.com>
2026-09-20 18:14:51 +0300

    serve static files and index page

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return RedirectResponse("/static/index.html")
